f0ba1c4cb86ddb36afce6d40fc6c81eb791c4d76
Moim HA <moimhossain@gmail.com>
2018-10-05 21:57:21 +0200

    Added API key

--- AzureDevOpsWiki/AzureDevOpsWiki/Controllers/WikiController.cs
+++ AzureDevOpsWiki/AzureDevOpsWiki/Controllers/WikiController.cs
@@ -25,19 +25,12 @@ namespace AzureDevOpsWiki.Controllers
 
         public WikiController()
         {
-            pat = "d7j623s4enrrzigdhzuk2oyqcguy2oq2x33t5inldadzqpxxi5bq";
-            accountName = "transport-logistics";
-            projectId = Guid.Parse("74d260f8-37a7-4580-83dd-f0fe2a608fb6");
-            repositoryId = Guid.Parse("390b2b76-0bbf-41d3-a17d-6198694ab069");
-            markdownServerUrl = "https://markdowntohtml.azurewebsites.net/";
-            sharedAccessSignature = "ZtvQEPptpoUEVy0MktRlGXK4Os6ses2r8d";
-
-            //pat = Env.GetEnvironmentVariable(Env.Keys.PAT);
-            //accountName = Env.GetEnvironmentVariable(Env.Keys.ORGANIZATION);
-            //projectId = Guid.Parse(Env.GetEnvironmentVariable(Env.Keys.PROJECTID));
-            //repositoryId = Guid.Parse(Env.GetEnvironmentVariable(Env.Keys.WIKIID));
-            //markdownServerUrl = Env.GetEnvironmentVariable(Env.Keys.MARKDOWNSERVICE);
-            //sharedAccessSignature = Env.GetEnvironmentVariable(Env.Keys.SAS);
+            pat = Env.GetEnvironmentVariable(Env.Keys.PAT);
+            accountName = Env.GetEnvironmentVariable(Env.Keys.ORGANIZATION);
+            projectId = Guid.Parse(Env.GetEnvironmentVariable(Env.Keys.PROJECTID));
+            repositoryId = Guid.Parse(Env.GetEnvironmentVariable(Env.Keys.WIKIID));
+            markdownServerUrl = Env.GetEnvironmentVariable(Env.Keys.MARKDOWNSERVICE);
+            sharedAccessSignature = Env.GetEnvironmentVariable(Env.Keys.SAS);
 
             client = new AzureDevOpsClient(accountName, pat);
         }
@@ -45,7 +38,7 @@ namespace AzureDevOpsWiki.Controllers
         [HttpGet("image")]
         public async Task<IActionResult> GetWikiImage(string path, string sas)
         {
-            if(!sharedAccessSignature.Equals( sas))
+            if (!sharedAccessSignature.Equals(sas))
             {
                 return new UnauthorizedResult();
             }
@@ -73,9 +66,9 @@ namespace AzureDevOpsWiki.Controllers
                 return new UnauthorizedResult();
             }
             var articleData = await client.GetWikiPageAsync(projectId, repositoryId, WebUtility.UrlEncode(pagePath), true);
-            
+
             var htmlData = await new Uri(markdownServerUrl).PostRestAsync<HtmlClass>("convert",
-                new MarkdownClass { Markdown = articleData.Content }, 
+                new MarkdownClass { Markdown = articleData.Content },
                 client.GetVstsHeaderFormatter());
 
             articleData.PageTitle = VstsWikiExtensions.ExtractTitle(articleData.Path);
